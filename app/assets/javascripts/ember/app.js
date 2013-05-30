a7fd985580a6a97ae3f5ee0dc7757c485346cdb4
Rob Anderson <rbndrsn1980@gmail.com>
2013-05-30 19:28:39 +0100

    fixed nav bar

--- app/assets/javascripts/ember/app.js
+++ app/assets/javascripts/ember/app.js
@@ -17,21 +17,30 @@ Noq.Store = DS.Store.extend({
 });
 
 Noq.User = DS.Model.extend({
-  name: DS.attr('string')
+  name: DS.attr('string'),
+  q_position: DS.attr('integer')
 });
 
 Noq.User.FIXTURES = [
   {
     id: 1,
-    name: "Joe"
+    name: "Joe",
+    q_position: 2
   },
   {
     id: 2,
-    name: "Betty"
+    name: "Betty",
+    q_position: 3
   },
   {
     id: 3,
-    name: "Marge"
+    name: "Marge",
+    q_position: 1
+  },
+  {
+    id: 4,
+    name: "Barney",
+    q_position: 4
   }
 ];
 
