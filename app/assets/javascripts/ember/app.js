1095954822ed99b6bcf4ebc2bbf1ebab1fc3cd48
Rob Anderson <rbndrsn1980@gmail.com>
2013-06-04 16:56:37 +0100

    added name validation, alphanumeric and spaces

--- app/assets/javascripts/ember/app.js
+++ app/assets/javascripts/ember/app.js
@@ -5,17 +5,12 @@
 //= require ./controllers/usersController
 
 Noq = Ember.Application.create({
-  LOG_TRANSITIONS: true
-
-});
-
-AlphaNumField:  Ember.TextField.extend({
+  LOG_TRANSITIONS: true,
+  AlphaNumField: Ember.TextField.extend({
     isValid: function() {
-      return /^[a-z0-9]+$/i.test(this.get('name'));
-      }.property('name'),
-      classNameBindings: 'isValid:valid:invalid'
+        return /^([A-Za-z0-9]+ ?)*$/i.test(this.get('value'));
+    }.property('value'),
+    classNameBindings: 'isValid:valid:invalid'
+  })
 });
-
-
-
 
