20f83e7cccde23c6a0254a7f311f7419e3d93037
Rob Anderson <rbndrsn1980@gmail.com>
2013-06-04 17:13:59 +0100

    added mobile and email validation to form

--- app/assets/javascripts/ember/app.js
+++ app/assets/javascripts/ember/app.js
@@ -11,6 +11,20 @@ Noq = Ember.Application.create({
         return /^([A-Za-z0-9]+ ?)*$/i.test(this.get('value'));
     }.property('value'),
     classNameBindings: 'isValid:valid:invalid'
-  })
+  }),
+
+  PhoneField: Ember.TextField.extend({
+    isValid: function() {
+        return /^([0-9]+ ?)*$/i.test(this.get('value'));
+    }.property('value'),
+    classNameBindings: 'isValid:valid:invalid'
+  }),
+
+  EmailField: Ember.TextField.extend({
+    isValid: function() {
+        return /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i.test(this.get('value'));
+    }.property('value'),
+    classNameBindings: 'isValid:valid:invalid'
+  }),
 });
 
