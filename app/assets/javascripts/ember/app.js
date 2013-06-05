6926657462c4b84269ba252bff8054902822fb2c
Rob Anderson <rbndrsn1980@gmail.com>
2013-06-05 14:35:49 +0100

    fix validations

--- app/assets/javascripts/ember/app.js
+++ app/assets/javascripts/ember/app.js
@@ -14,13 +14,13 @@ Noq = Ember.Application.create({
   }),
   MobileField: Ember.TextField.extend({
     isValid: function() {
-        return /^([A-Za-z0-9]+ ?)*$/i.test(this.get('value'));
+        return /^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$/i.test(this.get('value'));
     }.property('value'),
     classNameBindings: 'isValid:valid:invalid'
   }),
   EmailField: Ember.TextField.extend({
     isValid: function() {
-        return /^([A-Za-z0-9]+ ?)*$/i.test(this.get('value'));
+        return /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i.test(this.get('value'));
     }.property('value'),
     classNameBindings: 'isValid:valid:invalid'
   }),
