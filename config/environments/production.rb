1d795990ca3a6bdb7429f7b62082db6c27952267
Rob Anderson <rbndrsn1980@gmail.com>
2013-06-12 17:06:00 +0100

    Fixed silly extra end block

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Noq::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-    config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
@@ -74,5 +74,4 @@ Noq::Application.configure do
     :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => 'plain',
     :enable_starttls_auto => true  }
-end
 end
